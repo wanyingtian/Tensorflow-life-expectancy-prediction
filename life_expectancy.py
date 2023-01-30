ef1263267c229f5abd533fec2404c64e3a10f5d1
wanyingtian <wanying.tian@outlook.com>
2023-01-30 15:51:19 -0800

    data preprocessing

--- life_expectancy.py
+++ life_expectancy.py
@@ -14,3 +14,24 @@ dataset = pd.read_csv('Life_Expectancy_Data.csv')
 # print the dataset to visualize
 print(dataset.head())
 print(dataset.describe())
+# Drop the country and  continent columns as they shouldn't influence life expectancy 
+dataset = dataset.drop(['Country'], axis = 1)
+dataset = dataset.drop(['Continent'], axis = 1)
+#dataset = dataset.drop(['Status'], axis = 1)
+# # split data into label and features
+label_index=dataset.columns.get_loc("Life_expectancy ")
+labels = dataset.iloc[:,label_index] 
+features = dataset.drop(["Life_expectancy "], axis = 1)
+
+## Data Preprocessing
+# apply one-hot-encoding on all categorial columns
+features = pd.get_dummies(features)
+# split data into training and test sets
+features_train,features_test,labels_train,labels_test = train_test_split(features, labels, test_size=0.33, random_state=42)
+#standardize the numeric columns using ColumnTransformer
+numerical_features = features.select_dtypes(include=['float64','int64'])
+numerical_columns = numerical_features.columns
+ct = ColumnTransformer([("only numeric", StandardScaler(), numerical_columns)], remainder='passthrough')
+features_train_scaled = ct.fit_transform(features_train)
+features_test_scaled = ct.fit_transform(features_test)
+print('done')
