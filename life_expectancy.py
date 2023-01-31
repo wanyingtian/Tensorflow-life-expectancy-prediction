f493022a1c201ee2b96a0e5a1586d1102889dbc1
wanyingtian <wanying.tian@outlook.com>
2023-01-30 16:21:57 -0800

    built the model, optimized and evaluated

--- life_expectancy.py
+++ life_expectancy.py
@@ -3,14 +3,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import Normalizer
 from sklearn.compose import ColumnTransformer
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import InputLayer
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+import tensorflow
+import keras
+from tensorflow import keras 
+from keras.models import Sequential
+from keras.layers import InputLayer
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 ## Data Loading and Observing
 # Load the data csv file to dataset
 dataset = pd.read_csv('Life_Expectancy_Data.csv')
+dataset = dataset.dropna()
 # print the dataset to visualize
 print(dataset.head())
 print(dataset.describe())
@@ -24,6 +28,7 @@ labels = dataset.iloc[:,label_index]
 features = dataset.drop(["Life_expectancy "], axis = 1)
 
 ## Data Preprocessing
+print("Data preprocessing...")
 # apply one-hot-encoding on all categorial columns
 features = pd.get_dummies(features)
 # split data into training and test sets
@@ -36,6 +41,7 @@ features_train_scaled = ct.fit_transform(features_train)
 features_test_scaled = ct.fit_transform(features_test)
 
 ## Building the model
+print("Building the model...")
 # create an instance of sequential model
 my_model = Sequential()
 # create input layer, shape is number of features in dataset
@@ -46,7 +52,22 @@ my_model.add(input)
 # WHY 64
 my_model.add(Dense(64, activation = 'relu'))
 # add output layer - single output regression, 1 neuron
-# my_model.add(Dense(1))
+my_model.add(Dense(1))
 # print model summary
 print(my_model.summary())
+
+## Initializing Optimizer, and compiling model
+print("Initializing Optimizer, and compiling model..")
+# Create an instance of the Adam optimizer with the learning rate equal to 0.01.
+opt = Adam(learning_rate = 0.001)
+# Compile: loss use the Mean Squared Error (mse);  metrics use the Mean Absolute Error (mae)
+my_model.compile(loss = 'mse', metrics = ['mae'], optimizer = opt)
+
+## Fit and Evaluate the model
+print("Fitting the model...")
+my_model.fit(features_train_scaled, labels_train, epochs=40, batch_size = 1, verbose = 1)
+
+print("Evaluating the model...")
+res_mse, res_mae = my_model.evaluate(features_test_scaled, labels_test, verbose = 0)
+print("Final loss is:" + str(res_mse) + ", final metric is:" + str(res_mae))
 print('done')
